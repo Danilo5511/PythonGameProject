b8be19c349941dc8c5be42fac31921ea8d8c5200
Danilo5511 <daniloasl@outlook.com>
2024-03-15 20:55:40 -0300

    Criação de Diagramas

---
+++ DiagramaClassJogo/Class1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Class1(Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
---
+++ DiagramaClassJogo/EntityFactory.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class EntityFactory:
+    def __init__(self):
+        self.get_entity = None
+
+    def Operation1(self, ):
+        pass
+
